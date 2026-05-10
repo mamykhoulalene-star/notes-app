b2bc52141488998797f2cbf1ca6475a6b0b6e4ae
Mamy Khoulalene <mamykhoulalene@gmail.com>
2026-05-10 10:59:39 +0100

    second  commit

--- src/pages/Notes.jsx
+++ src/pages/Notes.jsx
@@ -2,9 +2,8 @@ import { useState,useEffect } from 'react';
 const Notes=()=>{
 
 const[note,setNote]=useState('')
-const[notes,setNotes]=
-    useState(()=>JSON.parse(localStorage.getItem('notes')||'[]')
-)
+const[notes,setNotes]=useState(()=>JSON.parse(localStorage.getItem('notes')||'[]'))
+const[search,setSearch]=useState('')
 
 useEffect(()=>{
     localStorage.setItem('notes',JSON.stringify(notes))
@@ -21,9 +20,17 @@ function deleteNote(note){
     setNotes(notes.filter((n)=>n.id!==note.id))
 }
 
+function handleSearch(){
+    const filterdNotes=notes.filter((n)=>n.note.toLowerCase().includes(search.toLowerCase()))
+    setNotes(filterdNotes)
+}
+
     return(
         <div>
             <h1>Notes</h1>
+<input type='text' placeholder='Search Notes' value={search} onChange={(e)=>setSearch(e.target.value)} onKeyDown={(e)=>e.key==='Enter'&&handleSearch()}/>
+
+
 <input type="text" 
 placeholder="Enter your Note here" 
 className="border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500"
@@ -37,22 +44,19 @@ onClick={addNote}
 >
               Add Note
             </button>
-<div className="mt-4">
+<ul className="mt-4">
     {notes.map((n)=>{
-        return <div>
-            <li key={n.id}>{n.note}
-                <button className="ml-2 mb-2 py-2 px-2 bg-red-500 text-white rounded-md 
+        return <li key={n.id}>{n.note}
+            <button className="ml-2 mb-2 py-2 px-2 bg-red-500 text-white rounded-md 
         hover:bg-red-600 focus:outline-none focus:ring-2 focus:ring-red-500"
-        onClick={()=>deleteNote(n)}
-        >
-          Delete
-        </button>
-            </li>
-        
-        </div>
+            onClick={()=>deleteNote(n)}
+            >
+              Delete
+            </button>
+        </li>
     })}
     
-    </div>
+</ul>
 
           </div>
         
